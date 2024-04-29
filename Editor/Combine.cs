ae882b518adb26e2640cb4a703aaafa35e0922cd
Connor deBoer <connor.deboer04@gmail.com>
2024-04-29 17:13:40 -0600

    UVs still not working properly

--- Editor/Combine.cs
+++ Editor/Combine.cs
@@ -16,6 +16,8 @@ public class Combine
     private string _path;
     private List<GameObject> _gameObjects;
 
+    private Mesh _combinedMesh;
+
     public Combine(string path, List<GameObject> gameObjects)
     {
         _path = path;
@@ -34,10 +36,10 @@ public class Combine
             combine[i].transform = _filters[i].transform.localToWorldMatrix;
         }
 
-        Mesh combinedMesh = new Mesh();
-        combinedMesh.CombineMeshes(combine);
+        _combinedMesh = new Mesh();
+        _combinedMesh.CombineMeshes(combine);
 
-        SaveAssets.SaveFile($"{_path}.asset", combinedMesh);
+        SaveAssets.SaveFile($"{_path}_Mesh.asset", _combinedMesh);
     }
 
     public void CombineTexture()
@@ -46,20 +48,50 @@ public class Combine
         int powerOfTwo = GetTextureSize(_renderer);
         int size = powerOfTwo * originalSize;
 
-        HashSet<Texture2D> baseMap = new HashSet<Texture2D>();
+        HashSet<Texture2D> baseMaps = new HashSet<Texture2D>();
         Texture2D combinedBaseMap = new Texture2D(size, size, TextureFormat.ARGB32, false);
 
         for (int i = 0; i < _renderer.Length; ++i)
         {
-            baseMap.Add((Texture2D)_renderer[i].sharedMaterial.mainTexture);
+            baseMaps.Add((Texture2D)_renderer[i].sharedMaterial.mainTexture);
         }
 
-        Rect[] uvCoordinates = combinedBaseMap.PackTextures(baseMap.ToArray(), 0, size);
+        Rect[] uvCoordinates = combinedBaseMap.PackTextures(baseMaps.ToArray(), 0, size);
 
         SaveAssets.SaveFile($"{_path}_Base.png", combinedBaseMap);
+
+        UpdateUV(uvCoordinates, baseMaps);
     }
 
-    private void UpdateUV()
+    private void UpdateUV(Rect[] uvCoordinates, HashSet<Texture2D> baseMaps)
+    {
+        // Build UV offset table
+        Dictionary<Texture2D, Rect> uvOffsetLookupTable = new Dictionary<Texture2D, Rect>();
+        for (int i = 0; i < uvCoordinates.Length; ++i)
+        {
+            uvOffsetLookupTable.Add(baseMaps.ElementAt(i), uvCoordinates[i]);
+        }
+
+        // Calculate new UVs
+        Vector2[] meshUvs = new Vector2[_combinedMesh.vertices.Length];
+
+        Rect offset;
+        for (int i = 0; i < _filters.Length; ++i)
+        {
+            offset = uvOffsetLookupTable[(Texture2D)_renderer[i].sharedMaterial.mainTexture];
+            for (int j = 0; j < _filters[i].sharedMesh.vertices.Length; ++j)
+            {
+                float x = (_filters[i].sharedMesh.uv[j].x * offset.width) + offset.x;
+                float y = (_filters[i].sharedMesh.uv[j].y * offset.height) + offset.y;
+                meshUvs[i] = new Vector2(x, y);
+            }
+        }
+
+        // Apply new UVs
+        _combinedMesh.uv = meshUvs;
+    }
+
+    private void GenerateMaterials()
     {
 
     }
